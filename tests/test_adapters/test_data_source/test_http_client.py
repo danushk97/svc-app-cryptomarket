0f6379fe5d5109ffa14975d558a7862f327058c8
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 19:56:19 +0530

    asserted for externalServiceException

--- tests/test_adapters/test_data_source/test_http_client.py
+++ tests/test_adapters/test_data_source/test_http_client.py
@@ -3,7 +3,7 @@ import pytest
 from tests.fakes.fake_requests import FakeFailureRequests, FakeSuccessRequests,\
     FakeFailureRequestsWithHttpError
 from src.adapters.data_source.http_client import HttpClient
-from src.exception import AppException
+from src.adapters.data_source.exception import ExternalServiceException
 
 
 def test_http_get_returns_response_data_on_successful_request():
@@ -14,20 +14,20 @@ def test_http_get_returns_response_data_on_successful_request():
 
 
 def test_http_get_raises_app_exception_on_unexpected_error():
-    with pytest.raises(AppException) as exc:
+    with pytest.raises(ExternalServiceException) as exc:
         HttpClient(FakeFailureRequests).get('http://test.com', '/')
 
     assert exc._excinfo[1].status == 500
 
 
 def test_http_get_raises_app_exception_on_unsuccessful_request(caplog):
-    with pytest.raises(AppException) as exc:
+    with pytest.raises(ExternalServiceException) as exc:
         HttpClient(FakeFailureRequestsWithHttpError).get('http://test.com', '/')
 
     exc = exc._excinfo[1]
     logs = [record.getMessage() for record in caplog.records]
     expected_log_message = \
         "[UNSUCCESSFUL EXTERNAL SERVICE CALL]: Error. {\"data\": \"mock\"}"
-    
+
     assert exc.status == 500
     assert expected_log_message in logs
