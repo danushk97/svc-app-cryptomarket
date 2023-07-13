bd06eacf0c19a5da3b0ae2e018e2e03ebd49a579
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 15:15:19 +0530

    added exception class

--- src/exception.py
+++ src/exception.py
@@ -1,18 +1,34 @@
+"""
+This module provides base exception class for the custom application exception.
+"""
+
 from http import HTTPStatus
 
 from src.error_codes import ErrorCode
 
 
 class AppException(Exception):
+    """
+    The `AppException` class serves as a base class for custom exceptions
+    that can be raised within the application.
+
+    Attributes:
+        title (str): A short, human-readable summary of the problem type.
+        detail (str): An human readable explanation specific to this occurrence of the problem.
+        status (HTTPStatus): The HTTP status code.
+        type (str): A URI that identifies the problem type or "about:blank"
+    """
     def __init__(
         self, 
-        title=ErrorCode.INTERNAL_SERVER_ERROR,
-        detail="Unknown error has occured",
-        status=HTTPStatus.INTERNAL_SERVER_ERROR,
-        type="about:blank"
+        title: str = ErrorCode.INTERNAL_SERVER_ERROR.value,
+        detail: str = "Unknown error has occured",
+        status : HTTPStatus = HTTPStatus.INTERNAL_SERVER_ERROR,
+        type: str = "about:blank",
+        log_message: str = ''
     ) -> None:
         self.title = title
         self.detail = detail
         self.status = status
         self.type = type
+        self.__log_message = log_message
     
