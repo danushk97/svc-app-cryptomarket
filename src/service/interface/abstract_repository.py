e2389d64c6142061375f174c0dd9222652012266
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 15:57:35 +0530

    removed data_source attribute from AbstractRepository class

--- src/service/interface/abstract_repository.py
+++ src/service/interface/abstract_repository.py
@@ -9,13 +9,7 @@ class AbstractRepository(ABC):
     """
     The `AbstractRepository` class defines the common interface and behavior
     for interacting with an repository.
-
-    Attributes:
-        data_source: Any class or instance that is responsible to fetch data.
     """
-
-    def __init__(self, data_source: str) -> None:
-        self.data_source = data_source
     
     @abstractmethod
     def list(self):
