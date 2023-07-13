28128dec0899b19f3176a14ab1cb7a05d7d4aeb8
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-14 00:52:58 +0530

    added test case to assert retrieve_all crypto market summary

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,41 @@
 import pytest
 
 from src.app import create_app
+from src.domain.crypto_market_summary import CryptoMarketSummary
 
 
 @pytest.fixture(scope="session")
 def test_client():
     app = create_app()
     yield app.test_client()
+
+
+def make_crypto_market_summary():
+    market_dict = {
+        "symbol": "1ECO-BTC",
+        "high": "0.000010130000",
+        "low": "0.000009550000",
+        "volume": "299.05366098",
+        "quoteVolume": "0.00286879",
+        "percentChange": "6.07",
+        "updatedAt": "2023-07-13T04:39:44.69Z"
+    }
+    return CryptoMarketSummary.from_dict(market_dict)
+
+
+@pytest.fixture(scope="session")
+def crypto_market_summary_instace():
+    return make_crypto_market_summary()
+
+
+@pytest.fixture(scope="session")
+def snake_case_market_summary_dict():
+    return {
+        "symbol": "1ECO-BTC",
+        "high": "0.000010130000",
+        "low": "0.000009550000",
+        "volume": "299.05366098",
+        "quote_volume": "0.00286879",
+        "percent_change": "6.07",
+        "updated_at": "2023-07-13T04:39:44.69Z"
+    }
--- tests/test_controller/test_crypto_market_summary.py
+++ tests/test_controller/test_crypto_market_summary.py
@@ -1,7 +1,24 @@
-def test_get_crypto_currency_market_summary_returns_ok_response(test_client):
-    response = test_client.get('/crypto/markets/summaries')
+import pytest
 
+from tests.fakes.fake_service import FakeCryptoMarketSummaryService
+
+
+service_path = "src.controller.crypto_market_summary.CryptoMarketSummaryService"
+
+
+@pytest.fixture()
+def fake_service(monkeypatch):
+    monkeypatch.setattr(
+        service_path, FakeCryptoMarketSummaryService
+    )
+
+
+def test_get_crypto_currency_market_summary_returns_ok_response(
+    fake_service, test_client, snake_case_market_summary_dict
+):
+
+    response = test_client.get("/crypto/markets/summaries")
+    response_json = response.get_json()
     assert response.status_code == 200
-    assert response.get_json() == {
-        "status": "UP"
-    }
+    assert len(response_json) == 1
+    assert response_json[0] == snake_case_market_summary_dict
