399f4a2803447be978062c9911cd5be8636fb9cf
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 15:43:00 +0530

    added test case for repo layer

---
+++ tests/test_adapters/test_repository/test_crypto_market_summary_http_repository.py
@@ -0,0 +1,24 @@
+from unittest.mock import create_autospec
+
+from src.adapters.repository.crypto_market_summary_http_repository import \
+    CryptoMarketSummaryHttpRepository
+from src.adapters.http_client import HttpClient
+
+
+def test_get_retrieve_crypto_market_summary_returns_list_of_market_summary():
+    market_summary_dict = {
+        "symbol": "1ECO-BTC",
+        "high": "0.000010130000",
+        "low": "0.000009550000",
+        "volume": "299.05366098",
+        "quoteVolume": "0.00286879",
+        "percentChange": "6.07",
+        "updatedAt": "2023-07-13T04:39:44.69Z"
+    }
+    mock_http_client = create_autospec(HttpClient, instance=True)
+    mock_http_client.get.return_value = [market_summary_dict]
+
+    crypto_market_summary_list = CryptoMarketSummaryHttpRepository(
+        mock_http_client
+    ).list()
+    assert crypto_market_summary_list == [market_summary_dict]
