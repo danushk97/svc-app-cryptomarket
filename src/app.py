708a3cfb1d4e7d23befab8112a481f63ae29b900
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 10:10:19 +0530

    created app.py

---
+++ src/app.py
@@ -0,0 +1,13 @@
+import os
+
+import connexion
+
+
+def create_app():
+    """
+    Creates and configures flask app using connexion.
+    """
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    connex_app = connexion.App(__name__, specification_dir=basedir)
+    
+    return connex_app.app
