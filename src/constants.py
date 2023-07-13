c60cb2761108e8c91d22f9f689630b60eaf927e7
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 11:55:11 +0530

    constants.py

---
+++ src/constants.py
@@ -0,0 +1,23 @@
+"""
+This module is responsible for holding constants that are used application wide.
+"""
+
+from typing import Any
+
+
+class Constants:
+
+    #  CryptoMarketSummary domain attributes
+    SYMBOL = "symbol"
+    HIGH = "high"
+    LOW = "low"
+    VOLUME = "volume"
+    QUOTE_VOLUME = "quote_volume"
+    UPDATED_AT = "updated_at"
+    PERCENT_CHANGE = "percent_change"
+
+    @staticmethod
+    def snake_to_camel_case(value: str) -> str:
+        first_value_part, *value_parts = value.split('_')
+
+        return first_value_part + ''.join(value.title() for value in value_parts)
