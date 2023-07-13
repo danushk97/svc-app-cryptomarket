39f337782a656f1690a2ae3372c95186e5f53af6
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 10:40:13 +0530

    fixed naming convention

--- tests/test_domain/test_crypto_market_summary.py
+++ tests/test_domain/test_crypto_market_summary.py
@@ -12,10 +12,10 @@ def test_instantiate_crypto_market_summary_class_returns_instance_with_values():
         "2023-07-13T04:39:44.69Z"
     )
     crypto_market_sumary = CryptoMarketSummary(*args)
-    assert crypto_market_sumary.summary == args[0]
+    assert crypto_market_sumary.symbol == args[0]
     assert crypto_market_sumary.high == args[1]
     assert crypto_market_sumary.low == args[2]
     assert crypto_market_sumary.volume == args[3]
-    assert crypto_market_sumary.quoteVolume == args[4]
-    assert crypto_market_sumary.percentChange == args[5]
-    assert crypto_market_sumary.updatedAt == args[6]
+    assert crypto_market_sumary.quote_volume == args[4]
+    assert crypto_market_sumary.percent_change == args[5]
+    assert crypto_market_sumary.updated_at == args[6]
