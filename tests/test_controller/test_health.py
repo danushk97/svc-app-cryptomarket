2c29b138f25448a1bbb3efbe89fdae24e42e1aab
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-14 00:53:56 +0530

    replace constant with literal

--- tests/test_controller/test_health.py
+++ tests/test_controller/test_health.py
@@ -1,10 +1,7 @@
-from http import HTTPStatus
-
-
 def test_health_check_returns_ok_response(test_client):
     response = test_client.get('/health-check')
 
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == 200
     assert response.get_json() == {
         "status": "UP"
     }
