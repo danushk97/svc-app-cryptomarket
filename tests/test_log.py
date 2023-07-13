38103a24292c1167bb6c3a998db787ccaa36d2f5
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 17:27:55 +0530

    asserted return value instance

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,9 +1,10 @@
-
+import logging
 
 from src.logs import get_logger
 
 
 def test_get_logger_returns_configured_logger_instance():
     logger = get_logger(__name__)
+    assert isinstance(logger, logging.Logger)
     assert logger.name == __name__
     assert logger.level == 10  # Logging Level for DEBUG
