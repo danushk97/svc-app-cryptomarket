b542a37d75dce7b243e6f74dc10abdeaf9911e7a
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 11:53:45 +0530

    added test case to convert snake to camel case

---
+++ tests/test_constants.py
@@ -0,0 +1,13 @@
+from pytest import mark
+
+from src.constants import Constants
+
+
+@mark.parametrize(
+    'input, expected', [
+        ('snake_case', 'snakeCase'),  # snake case should be converted to camel case
+        ('camelCase', 'camelCase')
+    ]
+)
+def test_to_camel_case_given_string_value_returns_string_in_camel_case(input, expected):
+    assert Constants.snake_to_camel_case(input) == expected
