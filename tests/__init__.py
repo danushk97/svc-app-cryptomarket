4f0fb44318e1dd49ce663290c52ff5183020ee0e
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 12:55:01 +0530

    added test case for abstract repo

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,5 @@
 from os import environ
 
+
 environ['FLASK_ENV'] = 'test'
+environ['BITTREX_SERVICE_BASE_URL'] = 'http://test-bittrex.com'
---
+++ tests/test_service/test_interface/test_abstract_repository.py
@@ -0,0 +1,22 @@
+import pytest
+
+from src.service.interface.abstract_repository import AbstractRepository
+
+
+
+class FakeRepo(AbstractRepository):
+    def list(self):
+        return super().list()
+
+    def find_by_summary(self, summary: str):
+        return super().find_by_summary(summary)
+
+
+def test_abstract_repo_raises_non_implemented_error():
+    fake_repo = FakeRepo('fake_data_source')
+    
+    with pytest.raises(NotImplementedError):
+        fake_repo.list()
+    
+    with pytest.raises(NotImplementedError):
+        fake_repo.find_by_summary('summary')
