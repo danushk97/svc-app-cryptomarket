41f310ee45c94072355d6f069eae5bd8665eb012
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 14:51:12 +0530

    updated tests

--- tests/test_exception.py
+++ tests/test_exception.py
@@ -3,8 +3,8 @@ from src.exception import AppException
 
 def test_app_exception_construction_returns_instance_with_deafault_init_values():
     exception = AppException()
-    assert exception.title == "Internal Server Error"
-    assert exception.detail == "Something went wrong"
+    assert exception.title.value == "Internal Server Error"
+    assert exception.detail == "Unknown error has occured"
     assert exception.type == "about:blank"
     assert exception.status == 500
-    assert exception.__cause == None
+    assert exception.__cause__ == None
