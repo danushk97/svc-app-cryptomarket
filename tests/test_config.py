5bb36862067f9b39cdf34834f834d39bf58649e9
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-13 20:15:47 +0530

    asserted logs on config load failure

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,12 +12,26 @@ def test_config_init_returns_none_if_all_required_configs_exists():
     assert Config.MARKET_SUMMARY_ENDPOINT == "/market/summary"
 
 
-@mark.parametrize('input', [
-    ({}),
-    ({'MARKET_SUMMARY_ENDPOINT': '/summary'})
+@mark.parametrize('input, expected_log', [
+    ({}, "Aborting application start up due to empty configs."),
+    (
+        {
+            'MARKET_SUMMARY_ENDPOINT': '/summary',
+            "ALL_MARKETS_SUMMARY_ENDPOINT": '/all-summaries'
+        },
+        (
+            "Aborting application start up due to missing configs "
+            "[BITTREX_SERVICE_BASE_URL]"
+        )
+    )
 ])
-def test_config_init_exit_app_on_empty_or_missing_configs(monkeypatch, input):
-    monkeypatch.setattr('src.config.dotenv_values', lambda x: {})
+def test_config_init_exit_app_on_empty_or_missing_configs(
+    monkeypatch, input, expected_log, caplog
+):
+    monkeypatch.setattr('src.config.dotenv_values', lambda x: input)
+    logs = [log.getMessage() for log in caplog.records]
+
     with patch('src.config._exit') as mock_exit:
         assert Config.init() is None
+        expected_log in logs
         mock_exit.assert_called_with(1)
