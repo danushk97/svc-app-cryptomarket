54bbb932e76def2d485fccfab655946dd15c5dfa
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-14 11:22:51 +0530

    added error handlers

--- src/app.py
+++ src/app.py
@@ -1,6 +1,14 @@
+"""
+This module provides the function that is responsible to create, configure
+and initialize FLASK APP.
+"""
+
 import os
 
 import connexion
+
+from src import error_handlers
+from src.exception import AppException
 
 
 def create_app():
@@ -10,5 +18,14 @@ def create_app():
     basedir = os.path.abspath(os.path.dirname(__file__))
     connex_app = connexion.App(__name__, specification_dir=basedir)
     connex_app.add_api("swagger.yml", strict_validation=True)
+
+    connex_app.app.register_error_handler(
+        AppException, 
+        error_handlers.app_error_handler
+    )
+    connex_app.app.register_error_handler(
+        Exception, 
+        error_handlers.generic_error_handler
+    )
     
     return connex_app.app
---
+++ src/error_handlers.py
@@ -0,0 +1,19 @@
+from src.exception import AppException
+from src.logs import get_logger
+
+
+_logger = get_logger(__name__)
+
+
+def app_error_handler(exception: AppException):
+    _logger.error(exception, exc_info=True)
+    
+    return exception.to_dict(), exception.status
+
+
+def generic_error_handler(exception: Exception):
+    _logger.error(exception, exc_info=True)
+
+    app_exception = AppException()
+    
+    return app_exception.to_dict(), app_exception.status 
