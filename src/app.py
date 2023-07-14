129319e6b61eb85c76c997d7a49a9cb20ba00c29
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-14 11:52:11 +0530

    init cofig

--- src/app.py
+++ src/app.py
@@ -9,12 +9,15 @@ import connexion
 
 from src import error_handlers
 from src.exception import AppException
+from src.config import Config
 
 
 def create_app():
     """
     Creates and configures flask app using connexion.
     """
+    Config.init()
+    
     basedir = os.path.abspath(os.path.dirname(__file__))
     connex_app = connexion.App(__name__, specification_dir=basedir)
     connex_app.add_api("swagger.yml", strict_validation=True)
