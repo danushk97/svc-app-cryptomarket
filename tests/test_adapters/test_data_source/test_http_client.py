8cdf96735db94175b666468e9d1cf8f4b107d720
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-15 14:18:59 +0530

    updated test to pass full url

--- tests/test_adapters/test_data_source/test_http_client.py
+++ tests/test_adapters/test_data_source/test_http_client.py
@@ -9,7 +9,7 @@ from src.adapters.data_source.exception import ExternalServiceException, \
 
 def test_http_get_returns_response_data_on_successful_request():
     data = HttpClient(FakeSuccessRequests()).get(
-        'http://test.com', '/'
+        'http://test.com/'
     )
     assert data == {
         'data': 'mock'
@@ -18,7 +18,7 @@ def test_http_get_returns_response_data_on_successful_request():
 
 def test_http_get_raises_app_exception_on_unexpected_error():
     with pytest.raises(ExternalServiceException) as exc:
-        HttpClient(FakeFailureRequests()).get('http://test.com', '/')
+        HttpClient(FakeFailureRequests()).get('http://test.com/')
 
     assert exc._excinfo[1].status == 500
 
@@ -26,7 +26,7 @@ def test_http_get_raises_app_exception_on_unexpected_error():
 def test_http_get_raises_app_exception_on_unsuccessful_request(caplog):
     with pytest.raises(ExternalServiceException) as exc:
         HttpClient(FakeFailureRequestsWithHttpError()).get(
-            'http://test.com', '/'
+            'http://test.com/'
         )
 
     exc = exc._excinfo[1]
@@ -41,7 +41,7 @@ def test_http_get_raises_app_exception_on_unsuccessful_request(caplog):
 def test_http_get_raises_resource_not_found_exception_on_404_response(caplog):
     with pytest.raises(ResourceNotFoundException) as exc:
         HttpClient(FakeFailureRequestsWithHttpError(404)).get(
-            'http://test.com', '/invalid-url'
+            'http://test.com/invalid-url'
         )
 
     exc = exc._excinfo[1]
