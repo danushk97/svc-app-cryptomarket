619390f6b7caf724be6e6df2309e73d521404e74
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-15 08:08:35 +0530

    fixed assertion data

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,7 +9,7 @@ def test_config_init_returns_none_if_all_required_configs_exists():
     assert Config.init() is None
     assert Config.BITTREX_SERVICE_BASE_URL == "http://test-base-url.com"
     assert Config.ALL_MARKETS_SUMMARY_ENDPOINT == "/summaries"
-    assert Config.MARKET_SUMMARY_ENDPOINT == "/market/summary"
+    assert Config.MARKET_SUMMARY_ENDPOINT == "/markets/{market}/summary"
 
 
 @mark.parametrize('input, expected_log', [
