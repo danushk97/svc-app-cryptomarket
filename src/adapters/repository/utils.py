84fcb94ffc41bcd5ca6958fd942332db1fed7a89
Danush Kumar Baskarraj <danushkumar.baskarraj@gmail.com>
2023-07-15 14:42:39 +0530

    docstrings and typehints

--- src/adapters/repository/utils.py
+++ src/adapters/repository/utils.py
@@ -1,15 +1,20 @@
+"""
+This module provides the utility funtions that are used inside repository
+modules.
+"""
+
 import hashlib
 import hmac
 
 
-def calculate_sha512_hash(value: str):
+def calculate_sha512_hash(value: str) -> str:
     sha512_hash = hashlib.sha512()
     sha512_hash.update(value.encode('utf-8'))
 
     return sha512_hash.hexdigest()
 
 
-def calculate_hmac_sha512_signature(data, secret_key):
+def calculate_hmac_sha512_signature(data: str, secret_key: str) -> str:
     hmac_digest = hmac.new(
         secret_key.encode('utf-8'), 
         data.encode('utf-8'), 
